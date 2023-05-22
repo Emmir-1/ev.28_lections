bb1b164237f7e4b60ae4d319142111f50f6feb9e
Emmir-1 <emmir.se@gmail.com>
2023-05-22 19:38:09 +0600

    new

---
+++ DB/intro.py
@@ -0,0 +1,175 @@
+# PostgreSQL - Система управление базами данных(СУБД/DBMS)
+# Это ряд программ и инструментов позволяющих создавать БД, управлять ею и манипулировать данными внутри(CRUD)
+
+# Posgres - сама база данных, она обектно реалиционая, то есть данные хранятся в виде таблиц, и имеют связь между собой
+
+# SQL (Stuctured Query Language) - декларативный язык структированных запросов, он применяется для создания и получение данных при помощи запросов БД 
+
+# -- -- -- -- -- -- -- -- -- -- -- -- 
+# Команда для входа в БД через юзера postgres:
+# sudo -u postgres psql
+
+# команда для входа 
+# exit
+
+# команда входа в своего юзера
+# psql
+
+# команда для выхода 
+# \q
+
+# Создание юзре
+#CREAT ROLE 'username' SUPERUSER LOGIN PASSWORD '1';  #CREATE DATABASE admin;
+
+# изменение пароля 
+# ALTER USER 'username' WITH PASSWORD '1';
+
+# создание БД 
+# CREAT DATABASE 'name';
+
+# \l - список всех БД
+
+# \du - все юзеры
+
+# \dt - все таблицы (нужно поключится к БД заранее)
+
+# \d 'name' - подробная информация к БД
+
+# \c 'name' - команда для поключение БД
+
+# psql -U <username> -d <dbname> - подключение под выбранным username к dbname
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Типы полей postgres
+
+# Numeric type(числовы типы)
+        # a. smallint(2 bytes) -> 32767 to 32767
+        # b. integer(4 bytes) -> 2.147... to 2.147...
+        # c. bigin(8 bytes) -> ... to ...
+        # d. real(4 bytes) -> число с плавающей точкой, вещественное
+        # f. double precsion (8 bytes) -> real но с двойной точностью
+        # d. serial (4 bytes) -> integer, auto-increment 
+
+
+# Character Types(Символьные строки(строковые)):
+    # a. varchar(кол-символов) -> если мы укажем 50 символов, а заполним только 10 то остальные будут свободны. Макс255
+    # b. char(кол-символов) -> если мы укажем 50 символов, а заполним только 10, то остальные будут заполнены пробелами. Макс 255
+# 'John'
+# 'John _ _ _ _ '
+    # c. text() -> неограниченое количество символов.
+
+# Boolen Type
+    # a. boolean (1 bytes) -> True/False
+
+# date -> календарная дата (год, месяц, день)
+
+# location -> координатная точка (x, y) - (245, -12)
+
+# enumerate types:
+    # ("a", "b", "c")
+    # CREAT TYPE <aby name> AS ENUM ("Happy", "Sad", "Mad");
+
+
+# - - - - - - - - - - - -- - - - - - - 
+# Команда для создания таблицы 
+# CREAT TABLE <tableName> (
+#     <column> <type>,
+# )
+
+# CREATE TABLE films (
+    # code char(5), 
+    # title varchar(100),
+    # date date,
+    # genre varchar(50),
+    # budget integer,
+    # country varchar(50),
+    # id serial
+    # );
+
+# DROP TABLE <name> - удаление таблиц
+
+# Команда для добавление данных в таблицу 
+# INSERT INTO <tablename> [(columns)] VALUES (data), (data);
+
+# INSERT INTO films(code, title, date, genre, budget, country) VALUES 
+# ('AU56', 'Game of Thrones', '2015-05-31', 'Fantasy', 1000000, 'USA');
+# ('het5', 'Lord of The Rings', '2001-06-12', 'Fantasy', 1200000, 'USA');
+
+# Команда для получение данных
+# SELECT (colums)* FROM <table>;
+
+# Команда для обнавление данных
+# UPDATE <table> SET <column> = <new_value> WHERE <column> = <value>;
+
+
+# Команда для удаление данных
+# DELET FROM <table> WHERE <column> = <value>;
+
+# ORDER BY: Он позволяет нам сортировать выводящие данные по убыванию или по возрастанию. ASC(по возрастанию) и DESC(по убыванию)
+# Синтаксис: SELECT <row> FROM <tabelname> ORDER BY <row> = [ASC/DESC];
+
+# WHERE: Используется для фильтраций по полям будут выводится только те данные которые соответсвуют условию оператору WHERE 
+# Синтаксис: SELECT <row> FROM <tabelname> WHERE <row> = "Чему либо";
+
+
+# BERWEEN: условие между
+# Синтакси: SELECT * FROM products WHERE id BETWEEN 3 and 8
+
+# LIKE: Выводит результат который соответсвует введеному шаблону для сторк. Чуствителен к ригистру 
+# ILEKE: Тоже самое но не зависит от ригистра
+# Синтаксис: SELECT <row> FROM <tabelname> WHERE <row> = LIKE/ILIKE 'чему либо';
+
+# AND: Оператор и, для множественых условий 
+# IN: WHERE <row> in (1, 2, 3, 4, 5);
+
+# LIMIT: Ставит ограниечение в кол-во получаемых данных
+
+# GROUP BY: разделяет данные котрые мы получаем в SELECT, при этом группируя их по
+# определеному признаку. И теперь для каждой группы можно использовать функцию
+
+# HAVING: ставит условия при помощи которго данные отбираются в группировка
+
+# Агрегатные функций: AVG(), COUNT(), MIN(), MAY(), SUM()
+
+# Экспорт БД(дамп): 
+# pg_dump -U <username> -d 'dbname' > 'file.sql'
+
+# Импорт:
+# psql -U <username> -d <dbname> -f <filename>
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Связи между таблицами(relations)
+    # 1.Один к одному (One to One) - человек и пасорт
+            # в одну из таблиц добавляется поле fk и дается ограничение unique
+    # 2. Один ко многим (One to Many) - человек и банковские карты  
+            # в таблицу много(банковских карты) доб. поле fk
+    # 3. Много ко многим (Many to Many) - Студенты и преподы
+            # создается вспомогательня третья таблица со связями
+
+
+# Ограничения :
+    # 1. NOT NULL - обезятельное к заполнению 
+    # 2. UNIQUE - то что будут хранится уникальные данные 
+    # 3. CHECK - CHECK age > 0 - ограничение проверки на условие 
+    # 4. PRIMERY KEY(для установки индетификатора данных в таблице)
+    # 5. FOREIGN KEY(для установки связей между таблицами)
+    # 6. ON DELET - для установки поведение при удаление данных которые были связоны 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# JOIN выборака данных из двух таблиц тоесть так назывемое соедине таблиц
+
+# LEFT JOIN выборак будет содержать все строки из левой таблицы 
+
+# RIGHT JOIN выборак будет содержать все строки из правой таблицы 
+
+# SELECT p1.title, p1.price, o1,quantity, p1.price * o1.quantity as
+# total_sum FROM products p1, orders o1 WHERE p1.id = o1.product_id;
+# - Запрос сразу две 
+
+# SELECT p1.title, p1.price, o1,quantity, p1.price * o1.quantity as
+# total_sum FROM products p1 JOIN orders o1 WHERE p1.id = o1.product_id ;
+
+
+
+
--- Home work/car.py
+++ Home work/car.py
@@ -1,182 +1,181 @@
-import json
+# import json
 
-class Cars:
-    def __init__(self, brand, model, year, engine_volume, color, body_type, mileage, price):
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.engine_volume = engine_volume
-        self.color = color
-        self.body_type = body_type
-        self.mileage = mileage
-        self.price = price
+# class Cars:
+#     def __init__(self, brand, model, year, engine_volume, color, body_type, mileage, price):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+#         self.engine_volume = engine_volume
+#         self.color = color
+#         self.body_type = body_type
+#         self.mileage = mileage
+#         self.price = price
 
-    def to_dict(self):
-        return {
-            'brand': self.brand,
-            'model': self.model,
-            'year': self.year,
-            'engine_volume': float(self.engine_volume),
-            'color': self.color,
-            'body_type': self.body_type,
-            'mileage': self.mileage,
-            'price': float(self.price)
-        }
-
-
-class CarsMixin:
-    def create(self, car):
-        cars = self.listing()
-        cars.append(car.to_dict())
-        self.save_cars(cars)
-
-    def listing(self):
-        cars = self.retrieve_all()
-        return cars
-
-    def retrieve(self, brand, model):
-        cars = self.retrieve_all()
-        for car in cars:
-            if car['brand'] == brand and car['model'] == model:
-                return car
-        return None
-
-    def update(self, brand, model, data):
-        cars = self.retrieve_all()
-        for car in cars:
-            if car['brand'] == brand and car['model'] == model:
-                car.update(data)
-                self.save_cars(cars)
-                return True
-        return False
-
-    def delete(self, brand, model):
-        cars = self.retrieve_all()
-        for car in cars:
-            if car['brand'] == brand and car['model'] == model:
-                cars.remove(car)
-                self.save_cars(cars)
-                return True
-        return False
-
-    def retrieve_all(self):
-        raise NotImplementedError
-
-    def save_cars(self, cars):
-        raise NotImplementedError
+#     def to_dict(self):
+#         return {
+#             'brand': self.brand,
+#             'model': self.model,
+#             'year': self.year,
+#             'engine_volume': float(self.engine_volume),
+#             'color': self.color,
+#             'body_type': self.body_type,
+#             'mileage': self.mileage,
+#             'price': float(self.price)
+#         }
 
 
-class CarsDatabase(CarsMixin):
-    def __init__(self, database_name):
-        self.database_name = database_name
+# class CarsMixin:
+#     def create(self, car):
+#         cars = self.listing()
+#         cars.append(car.to_dict())
+#         self.save_cars(cars)
 
-    def retrieve_all(self):
-        try:
-            with open(self.database_name, 'r') as file:
-                cars = json.load(file)
-        except FileNotFoundError:
-            cars = []
-        return cars
+#     def listing(self):
+#         cars = self.retrieve_all()
+#         return cars
 
-    def save_cars(self, cars):
-        with open(self.database_name, 'w') as file:
-            json.dump(cars, file, indent=4)
+#     def retrieve(self, brand, model):
+#         cars = self.retrieve_all()
+#         for car in cars:
+#             if car['brand'] == brand and car['model'] == model:
+#                 return car
+#         return None
+
+#     def update(self, brand, model, data):
+#         cars = self.retrieve_all()
+#         for car in cars:
+#             if car['brand'] == brand and car['model'] == model:
+#                 car.update(data)
+#                 self.save_cars(cars)
+#                 return True
+#         return False
+
+#     def delete(self, brand, model):
+#         cars = self.retrieve_all()
+#         for car in cars:
+#             if car['brand'] == brand and car['model'] == model:
+#                 cars.remove(car)
+#                 self.save_cars(cars)
+#                 return True
+#         return False
+
+#     def retrieve_all(self):
+#         raise NotImplementedError
+
+#     def save_cars(self, cars):
+#         raise NotImplementedError
 
 
-# Пример использования
+# class CarsDatabase(CarsMixin):
+#     def __init__(self, database_name):
+#         self.database_name = database_name
 
-database = CarsDatabase('cars_database.json')
+#     def retrieve_all(self):
+#         try:
+#             with open(self.database_name, 'r') as file:
+#                 cars = json.load(file)
+#         except FileNotFoundError:
+#             cars = []
+#         return cars
 
-while True:
-    print('--- Управление запасами автомобилей ---')
-    print('1. Add a Car') # Добавить автомобиль
-    print('2. List All Cars') # Список всех автомобилей
-    print('3. Retrieve a Car') # Извлечь автомобиль
-    print('4. Update a Car') # Обновление автомобиля
-    print('5. Delete a Car') # Удалить автомобиль
-    print('6. Exit') # Выход
+#     def save_cars(self, cars):
+#         with open(self.database_name, 'w') as file:
+#             json.dump(cars, file, indent=4)
 
-    choice = input('Enter your choice: ')
 
-    if choice == '1':
-        brand = input('Введите бренд: ')
-        model = input('Введите модель: ')
-        year = int(input('Введите год производства: '))
-        engine_volume = float(input('Введите объем двигателя: '))
-        color = input('Введите цвет: ')
-        body_type = input('Введите тип кузова: ')
-        mileage = int(input('Введите пробег: ')) 
-        price = float(input('Введите цену: '))  
+# # Пример использования
 
-        car = Cars(brand, model, year, engine_volume, color, body_type, mileage, price)
-        database.create(car)  # Добавление автомобиля в базу данных
+# database = CarsDatabase('cars_database.json')
 
-    elif choice == '2':
-        cars = database.listing()  # Получение списка всех автомобилей
-        if cars:
-            print('--- Список автомобилей ---')
-            for car in cars:
-                print(f"Brand: {car['brand']}, Model: {car['model']}, Year: {car['year']}, Price: {car['price']}")
-        else:
-            print('Автомобили не найдены.')
+# while True:
+#     print('--- Управление запасами автомобилей ---')
+#     print('1. Add a Car') # Добавить автомобиль
+#     print('2. List All Cars') # Список всех автомобилей
+#     print('3. Retrieve a Car') # Извлечь автомобиль
+#     print('4. Update a Car') # Обновление автомобиля
+#     print('5. Delete a Car') # Удалить автомобиль
+#     print('6. Exit') # Выход
 
-    elif choice == '3':
-        brand = input('Введите бренд: ')
-        model = input('Введите модель: ')
-        car = database.retrieve(brand, model)  # Получение информации об одном автомобиле
-        if car:
-            print('--- Извлеченный автомобиль ---')
-            print(f"Brand: {car['brand']}, Model: {car['model']}, Year: {car['year']}, Price: {car['price']}")
-        else:
-            print('Автомобиль не найден.')
+#     choice = input('Enter your choice: ')
 
-    elif choice == '4':
-        brand = input('Введите бренд: ')
-        model = input('Введите модель: ')
-        car = database.retrieve(brand, model)  # Получение информации об одном автомобиле
-        if car:
-            print('--- Обновить автомобиль ---')
-            print('Введите новую информацию (оставить пустым, чтобы сохранить существующий):')
-            new_brand = input(f"Brand ({car['brand']}): ") or car['brand']
-            new_model = input(f"Model ({car['model']}): ") or car['model']
-            new_year = int(input(f"Year ({car['year']}): ") or car['year'])
-            new_engine_volume = float(input(f"Engine Volume ({car['engine_volume']}): ") or car['engine_volume'])
-            new_color = input(f"Color ({car['color']}): ") or car['color']
-            new_body_type = input(f"Body Type ({car['body_type']}): ") or car['body_type']
-            new_mileage = int(input(f"Mileage ({car['mileage']}): ") or car['mileage'])
-            new_price = float(input(f"Price ({car['price']}): ") or car['price'])
+#     if choice == '1':
+#         brand = input('Введите бренд: ')
+#         model = input('Введите модель: ')
+#         year = int(input('Введите год производства: '))
+#         engine_volume = float(input('Введите объем двигателя: '))
+#         color = input('Введите цвет: ')
+#         body_type = input('Введите тип кузова: ')
+#         mileage = int(input('Введите пробег: ')) 
+#         price = float(input('Введите цену: '))  
 
-            updated_data = {
-                'brand': new_brand,
-                'model': new_model,
-                'year': new_year,
-                'engine_volume': new_engine_volume,
-                'color': new_color,
-                'body_type': new_body_type,
-                'mileage': new_mileage,
-                'price': new_price
-            }
+#         car = Cars(brand, model, year, engine_volume, color, body_type, mileage, price)
+#         database.create(car)  # Добавление автомобиля в базу данных
 
-            if database.update(brand, model, updated_data):  # Обновление информации об автомобиле
-                print('Автомобиль успешно обновлен.')
-            else:
-                print('Не удалось обновить автомобиль.')
-        else:
-            print('Автомобиль не найден.')
+#     elif choice == '2':
+#         cars = database.listing()  # Получение списка всех автомобилей
+#         if cars:
+#             print('--- Список автомобилей ---')
+#             for car in cars:
+#                 print(f"Brand: {car['brand']}, Model: {car['model']}, Year: {car['year']}, Price: {car['price']}")
+#         else:
+#             print('Автомобили не найдены.')
 
-    elif choice == '5':
-        brand = input('Введите бренд: ')
-        model = input('Введите модель: ')
-        if database.delete(brand, model):  # Удаление автомобиля
-            print('Автомобиль успешно удален.')
-        else:
-            print('Автомобиль не найден.')
+#     elif choice == '3':
+#         brand = input('Введите бренд: ')
+#         model = input('Введите модель: ')
+#         car = database.retrieve(brand, model)  # Получение информации об одном автомобиле
+#         if car:
+#             print('--- Извлеченный автомобиль ---')
+#             print(f"Brand: {car['brand']}, Model: {car['model']}, Year: {car['year']}, Price: {car['price']}")
+#         else:
+#             print('Автомобиль не найден.')
 
-    elif choice == '6':
-        break  # Выход из программы
+#     elif choice == '4':
+#         brand = input('Введите бренд: ')
+#         model = input('Введите модель: ')
+#         car = database.retrieve(brand, model)  # Получение информации об одном автомобиле
+#         if car:
+#             print('--- Обновить автомобиль ---')
+#             print('Введите новую информацию (оставить пустым, чтобы сохранить существующий):')
+#             new_brand = input(f"Brand ({car['brand']}): ") or car['brand']
+#             new_model = input(f"Model ({car['model']}): ") or car['model']
+#             new_year = int(input(f"Year ({car['year']}): ") or car['year'])
+#             new_engine_volume = float(input(f"Engine Volume ({car['engine_volume']}): ") or car['engine_volume'])
+#             new_color = input(f"Color ({car['color']}): ") or car['color']
+#             new_body_type = input(f"Body Type ({car['body_type']}): ") or car['body_type']
+#             new_mileage = int(input(f"Mileage ({car['mileage']}): ") or car['mileage'])
+#             new_price = float(input(f"Price ({car['price']}): ") or car['price'])
 
-    else:
-        print('Неверный выбор. Пожалуйста, попробуйте еще раз.')
+#             updated_data = {
+#                 'brand': new_brand,
+#                 'model': new_model,
+#                 'year': new_year,
+#                 'engine_volume': new_engine_volume,
+#                 'color': new_color,
+#                 'body_type': new_body_type,
+#                 'mileage': new_mileage,
+#                 'price': new_price
+#             }
 
+#             if database.update(brand, model, updated_data):  # Обновление информации об автомобиле
+#                 print('Автомобиль успешно обновлен.')
+#             else:
+#                 print('Не удалось обновить автомобиль.')
+#         else:
+#             print('Автомобиль не найден.')
+
+#     elif choice == '5':
+#         brand = input('Введите бренд: ')
+#         model = input('Введите модель: ')
+#         if database.delete(brand, model):  # Удаление автомобиля
+#             print('Автомобиль успешно удален.')
+#         else:
+#             print('Автомобиль не найден.')
+
+#     elif choice == '6':
+#         break  # Выход из программы
+
+#     else:
+#         print('Неверный выбор. Пожалуйста, попробуйте еще раз.')
 
 
